358c4bc016df9aad7d0dc0f8a49ec63e14f07326
evananann <196025301+evananann@users.noreply.github.com>
2026-01-22 04:37:01 +0800

    a
    modified:   src/main/java/DOG.java

--- src/main/java/DOG.java
+++ src/main/java/DOG.java
@@ -1,10 +1,13 @@
-public class Duke {
+public class DOG {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "----------------------------------------------------------"
+
+        System.out.println(line);
+        System.out.println("WOOF! I am DOG!");
+        System.out.println("How are you?");
+        System.out.println("What can I do for you today?");
+        System.out.println(line);
+        System.out.println("Bye! Catch you later!");
+
     }
 }
